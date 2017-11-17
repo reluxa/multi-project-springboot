7bc377fa326966b4996777bff314f98e0fa9b1ed
reluxa <reluxa@gmail.com>
2017-11-17 22:29:31 +0100

    compiles after dependency management addded

---
+++ services/helloboot-service/src/main/java/se/codemonkey/App.java
@@ -0,0 +1,18 @@
+package se.codemonkey;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+      return "Hello Boot";
+    }
+
+    public static void main(String[] args) throws Exception {
+      SpringApplication.run(App.class, args);
+    }
+}
---
+++ services/helloboot-service/src/test/java/se/codemonkey/AppTest.java
@@ -0,0 +1,10 @@
+package se.codemonkey;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test public void testAppHasAGreeting() {
+        App classUnderTest = new App();
+    }
+}
