8f920d91539d9238e1d48bb3002189a4729acbea
reluxa <reluxa@gmail.com>
2017-11-26 13:50:30 +0100

    servives are registerded into service registry

--- services/helloboot-service/src/main/java/org/reluxa/helloboot/App.java
+++ services/helloboot-service/src/main/java/org/reluxa/helloboot/App.java
@@ -1,10 +1,12 @@
 package org.reluxa.helloboot;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class App {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
--- services/hellospring-service/src/main/java/org/reluxa/hellospring/App.java
+++ services/hellospring-service/src/main/java/org/reluxa/hellospring/App.java
@@ -3,12 +3,14 @@ package org.reluxa.hellospring;
 import org.reluxa.ExampleUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class App {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
