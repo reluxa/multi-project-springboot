45464f00a054cd0862df5bed4c86ea624a45f8f1
reluxa <reluxa@gmail.com>
2017-11-27 08:06:03 +0100

    config-server rev #1 added

---
+++ infra/config-server/src/main/java/org/reluxa/configserver/App.java
@@ -0,0 +1,15 @@
+package org.reluxa.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class App {
+
+    public static void main(String[] args) throws Exception {
+      SpringApplication.run(App.class, args);
+    }
+
+}
---
+++ infra/config-server/src/test/java/org/reluxa/configserver/ConfigServerTest.java
@@ -0,0 +1,12 @@
+package org.reluxa.configserver;
+
+import org.junit.Test;
+import org.reluxa.configserver.App;
+
+public class ConfigServerTest {
+
+    @Test
+    public void testAppHasAGreeting() {
+        App classUnderTest = new App();
+    }
+}
